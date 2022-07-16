cbacc0303bb01c8d29f63a00fe4d49ef70313c43
Ethan Lim <ethanolicacid@outlook.com>
2022-07-16 10:54:24 +0800

    Initial Commit

---
+++ Meow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meow
+//
+//  Created by Ethan Lim on 16/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meow/MeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowApp.swift
+//  Meow
+//
+//  Created by Ethan Lim on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
