0bd12f582f5786c502e1d22ad93326d38498c8ec
Ethan Lim <ethanolicacid@outlook.com>
2022-07-16 10:56:19 +0800

    Added CatFactView and CatImageView
    Meow

---
+++ Meow/CatFactView.swift
@@ -0,0 +1,20 @@
+//
+//  CatFactView.swift
+//  Meow
+//
+//  Created by Ethan Lim on 16/7/22.
+//
+
+import SwiftUI
+
+struct CatFactView: View {
+    var body: some View {
+        Text("This is cat fact view!")
+    }
+}
+
+struct CatFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatFactView()
+    }
+}
---
+++ Meow/CatImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CatImageView.swift
+//  Meow
+//
+//  Created by Ethan Lim on 16/7/22.
+//
+
+import SwiftUI
+
+struct CatImageView: View {
+    var body: some View {
+        Text("This is cat image view")
+    }
+}
+
+struct CatImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatImageView()
+    }
+}
